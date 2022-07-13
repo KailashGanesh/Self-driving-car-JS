5683d4c34610360e2c6e938a17c582d7e5fc6f20
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2022-07-13 13:29:13 +0530

    arrow keys to move the car right left forward back

--- Js/car.js
+++ Js/car.js
@@ -5,26 +5,76 @@ class Car{
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+
+
         this.controls = new Controls();
     }
 
     update(){
         if (this.controls.forward){
-            this.y-=2;
+            this.speed += this.acceleration;
         }
         if (this.controls.reverse){
-            this.y+=2;
+            this.speed -= this.acceleration;
         }
+
+        if (this.speed > this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2;
+        }
+
+        // if speed is grate then zero, decrease it by friction - this should emulate friction
+        if(this.speed > 0){
+            this.speed -= this.friction;
+        }
+        // if we are going backwards, it will add friction thus slowing it down
+        if(this.speed < 0){
+            this.speed += this.friction;
+        }
+        if(Math.abs(this.speed) < this.friction){
+            this.speed = 0;
+        }
+
+        // only turns when there is some speed - car can't turn in place
+        if(this.speed != 0){
+            // are we going forwards or backwards?
+            const flip = this.speed>0? 1:-1;
+
+            // if going backward and pressing right, the car will move right since we have fixed the signs
+            if(this.controls.left){
+                this.angle += 0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle -= 0.03*flip;
+            }
+        }
+
+
+        this.x -= Math.sin(this.angle)*this.speed;
+
+        this.y -= Math.cos(this.angle)*this.speed;
     }
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+        
+        ctx.restore();
     }
 }
